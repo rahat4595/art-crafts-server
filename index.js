a052ce5d51d84639f0a2f69c76caa57c601a7e6e
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-04-27 13:54:58 +0600

    fetched data for view details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+  //  view details page
+  app.get('/crafts/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await craftsCollection.findOne(query);
+    res.send(result);
+  })
 
 
     // creating data to sever
